87911c8d016886601a4c81ef299ef30c3c23f719
Sergey Bolotnikov <sbolotnikov@gmail.com>
2025-10-28 05:57:38 -0400

    3 commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,20 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./global.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './global.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  "title": "React Arcade",
-  "description": "A collection of classic arcade games built with React. Choose your controls and aim for the high score in games like Tetris and Snake!",
+  title: 'Arcade Games',
+  description:
+    'A collection of classic arcade games built with React. Choose your controls and aim for the high score in games like Tetris and Snake!',
 };
 
 export default function RootLayout({
@@ -23,6 +24,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link
+        rel="icon"
+        href="/icon?<generated>"
+        type="image/<generated>"
+        sizes="<generated>"
+      />
+      <link
+        rel="apple-touch-icon"
+        href="/apple-icon?<generated>"
+        type="image/<generated>"
+        sizes="<generated>"
+      />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
